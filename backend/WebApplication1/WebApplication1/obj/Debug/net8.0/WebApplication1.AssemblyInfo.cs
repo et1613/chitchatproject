dba83f6470120ebfdfd1ab292514812547a64a3f
Emir Taç <taccemir.1@gmail.com>
2025-03-22 16:47:22 +0300

    Draw.io eklendi

--- backend/WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ backend/WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82ba9fd548ba73b6a1f53f33c1f873ffefc75b64")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c827d8e44583a9ebaf1fa1b1e124b9576a7e6729")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
